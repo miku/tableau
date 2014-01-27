5921fd523aff06980583bd31c6343ddd9a21ec8a
Martin Czygan <martin.czygan@gmail.com>
2014-01-27 21:11:25 +0100

    first rough draft of tableau

--- tableau/app.py
+++ tableau/app.py
@@ -1,11 +1,30 @@
 #!/usr/bin/env python
 
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
 app = Flask(__name__)
+
+SIMDB = ""
+
+@app.route("/unrated")
+def unrated():
+    return render_template('index.html', name='unrated')
+
+@app.route("/rated")
+def rated():
+    name = request.args.get('v', 'plus')
+    return render_template('index.html', name=name)
+
+@app.route("/summary")
+def summary():
+    return render_template('index.html', name='summary')
+
+@app.route("/search")
+def search():
+    return render_template('index.html', name='search')
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return redirect(url_for('summary'))
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0")
