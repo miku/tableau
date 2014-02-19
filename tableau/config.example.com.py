ecbfaa20a22e3fddbc69d81987a4024296b8f564
Martin Czygan <martin.czygan@gmail.com>
2014-02-19 13:50:57 +0100

    updated example config

--- tableau/config.example.com.py
+++ tableau/config.example.com.py
@@ -1,2 +1,4 @@
-SIMDB = "/path/to/output/of/similardb-task"
-BASE = "http://1.2.3.4:5000/doc"
+# SIM_DB = "/media/mtc/Data/tasktree-data/f3/similar-db/date-2014-02-06-indices-bsz-ebl-nep-size-3-sources-ebl-nep.db"
+SIM_DB = "/tmp/example.db"
+BASE = "http://0.0.0.0:5000/doc"
+FEEDBACK_DB = "/tmp/feedback.db"
